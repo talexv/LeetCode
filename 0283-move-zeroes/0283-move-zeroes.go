629dca3cf1ae9cbbcb9eea0a0a4d2bd87b777de8
talexv <120385790+talexv@users.noreply.github.com>
2025-03-11 21:44:47 +0500

    Time: 0 ms (100%), Space: 9.9 MB (7.12%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.go
@@ -0,0 +1,9 @@
+func moveZeroes(nums []int)  {
+    left := 0
+    for i := 0; i < len(nums); i++ {
+        if nums[i] != 0 {
+            nums[left], nums[i] = nums[i], nums[left]
+            left++
+        }
+    }
+}
