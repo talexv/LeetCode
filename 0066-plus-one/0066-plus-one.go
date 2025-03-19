dd90598c8ff2ead2d3965bd450403e9f64f8ce5a
talexv <talexvi@yandex.ru>
2025-03-20 02:21:21 +0500

    update

--- 0066-plus-one/0066-plus-one.go
+++ 0066-plus-one/0066-plus-one.go
@@ -1,14 +1,18 @@
+package plusone
+
 // Compute: O(n)
 // Memory: O(1)
 
+//nolint:unused // solution LeetCode problem
 func plusOne(digits []int) []int {
-    for i := len(digits) - 1; i >= 0; i-- {
-        if digits[i] < 9 {
-            digits[i]++
-            return digits
-        }
-        digits[i] = 0
-    }
-    digits = append([]int{1}, digits...)
-    return digits
+	maxDigit := 9
+	for i := len(digits) - 1; i >= 0; i-- {
+		if digits[i] < maxDigit {
+			digits[i]++
+			return digits
+		}
+		digits[i] = 0
+	}
+	digits = append([]int{1}, digits...)
+	return digits
 }
