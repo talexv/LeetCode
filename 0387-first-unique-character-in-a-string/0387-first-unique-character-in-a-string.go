83ac6d95c6ceda727b1720bd65adf334e8deadbc
talexv <120385790+talexv@users.noreply.github.com>
2025-03-23 19:24:51 +0500

    Time: 23 ms (62.47%), Space: 7.8 MB (89.37%) - LeetHub

---
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.go
@@ -0,0 +1,12 @@
+func firstUniqChar(s string) int {
+    m := make(map[rune]int)
+    for _, char := range s {
+        m[char]++
+    }
+    for i, char := range s {
+        if m[char] == 1 {
+            return i
+        }
+    }
+    return -1
+}
