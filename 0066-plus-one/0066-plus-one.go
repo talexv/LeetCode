8541b0b6f94c5928f7da8b4afff45f63b7a5d72a
talexv <talexvi@yandex.ru>
2025-03-24 20:31:56 +0500

    Update

--- 0066-plus-one/0066-plus-one.go
+++ 0066-plus-one/0066-plus-one.go
@@ -6,14 +6,15 @@ package plusone
 //nolint:unused // solution LeetCode problem
 func plusOne(digits []int) []int {
 	maxDigit := 9
-	for i := len(digits) - 1; i >= 0; i-- {
+	n := len(digits)
+	digits = append(digits, 0)
+	for i := n - 1; i >= 0; i-- {
 		if digits[i] < maxDigit {
 			digits[i]++
-			return digits
+			return digits[:n]
 		}
 		digits[i] = 0
 	}
-	result := make([]int, len(digits)+1)
-	result[0] = 1
-	return result
+	digits[0] = 1
+	return digits
 }
