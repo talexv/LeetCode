1e8ab12d37033b2ed0385eec01bff8e8efd9d71a
talexv <talexvi@yandex.ru>
2025-03-24 12:56:10 +0500

    update

--- 0125-valid-palindrome/0125-valid-palindrome.go
+++ 0125-valid-palindrome/0125-valid-palindrome.go
@@ -1,18 +1,25 @@
-func isPalindrome(s string) bool {
-    var chars []rune
+package validpalindrome
 
-    for _, char := range s {
-        if unicode.IsLetter(char) || unicode.IsDigit(char) {
-            chars = append(chars, unicode.ToLower(char))
-        }
-    }
-    left, right := 0, len(chars) - 1
-    for left < right {
-        if chars[left] != chars[right] {
-            return false
-        }
-        left++
-        right--
-    }
-    return true
+import "unicode"
+
+// Compute: O(n)
+// Memory: O(n)
+
+//nolint:unused // solution LeetCode problem
+func isPalindrome(s string) bool {
+	var chars []rune
+	for _, char := range s {
+		if unicode.IsLetter(char) || unicode.IsDigit(char) {
+			chars = append(chars, unicode.ToLower(char))
+		}
+	}
+	left, right := 0, len(chars)-1
+	for left < right {
+		if chars[left] != chars[right] {
+			return false
+		}
+		left++
+		right--
+	}
+	return true
 }
--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.go
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.go
@@ -1,12 +1,18 @@
+package firstuniquecharacterinastring
+
+// Compute: O(n)
+// Memory: O(n)
+
+//nolint:unused // solution LeetCode problem
 func firstUniqChar(s string) int {
-    m := make(map[rune]int)
-    for _, char := range s {
-        m[char]++
-    }
-    for i, char := range s {
-        if m[char] == 1 {
-            return i
-        }
-    }
-    return -1
+	m := make(map[rune]int)
+	for _, char := range s {
+		m[char]++
+	}
+	for i, char := range s {
+		if m[char] == 1 {
+			return i
+		}
+	}
+	return -1
 }
