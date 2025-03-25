5f06c489a2e4ac8645ceb3287808b9b371122ddb
talexv <talexvi@yandex.ru>
2025-03-25 16:25:47 +0500

    Update

--- 0014-longest-common-prefix/0014-longest-common-prefix.go
+++ 0014-longest-common-prefix/0014-longest-common-prefix.go
@@ -1,15 +1,21 @@
+package longestcommonprefix
+
+// Compute: O(n*m)
+// Memory: O(m)
+
+//nolint:unused // solution LeetCode problem
 func longestCommonPrefix(strs []string) string {
 	if len(strs) == 0 {
 		return ""
 	}
-    prefix := strs[0]
-    for i := 1; i < len(strs); i++ {
-        for len(strs[i]) < len(prefix) || strs[i][:len(prefix)] != prefix {
-            prefix = prefix[:len(prefix)-1]
-            if prefix == ""{
-                return ""
-            }
-        }
-    }
-    return prefix
+	prefix := strs[0]
+	for i := 1; i < len(strs); i++ {
+		for len(strs[i]) < len(prefix) || strs[i][:len(prefix)] != prefix {
+			prefix = prefix[:len(prefix)-1]
+			if prefix == "" {
+				return ""
+			}
+		}
+	}
+	return prefix
 }
--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.go
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.go
@@ -1,8 +1,14 @@
+package findtheindexofthefirstoccurrenceinastring
+
+// Compute: O(n*m)
+// Memory: O(1)
+
+//nolint:unused // solution LeetCode problem
 func strStr(haystack string, needle string) int {
-    for i := 0; i <= len(haystack) - len(needle); i++ {
-        if haystack[i:i+len(needle)] == needle {
-            return i
-        }
-    }
-    return -1
+	for i := 0; i <= len(haystack)-len(needle); i++ {
+		if haystack[i:i+len(needle)] == needle {
+			return i
+		}
+	}
+	return -1
 }
