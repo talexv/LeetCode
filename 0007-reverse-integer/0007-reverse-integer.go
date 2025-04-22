f8d1b9efc7ef7e507f9acd10575aa8e6796d23f7
talexv <120385790+talexv@users.noreply.github.com>
2025-04-23 00:27:12 +0500

    Time: 2 ms (36.49%), Space: 4 MB (25.16%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.go
@@ -0,0 +1,28 @@
+func reverse(x int) int {
+	sign := 1
+
+	if x < 0 {
+		sign = -1
+		x *= sign
+	}
+
+	s := strconv.Itoa(x)
+	r := []rune(s)
+	for i, j := 0, len(r) - 1; i < j; {
+		r[i], r[j] = r[j], r[i]
+		i++
+		j--
+	}
+
+	res, err := strconv.Atoi(string(r))
+	if err != nil {
+		return 0
+	}
+
+	res *= sign
+	if res > math.MaxInt32 || res < math.MinInt32 {
+		return 0
+	}
+
+	return res
+}
