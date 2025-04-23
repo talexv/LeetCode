d00f015b5e1ff7f8a4f42a3692802ce954b1d118
talexv <talexvi@yandex.ru>
2025-04-23 14:07:25 +0500

    Update

--- 0007-reverse-integer/0007-reverse-integer.go
+++ 0007-reverse-integer/0007-reverse-integer.go
@@ -1,3 +1,14 @@
+package reverseinteger
+
+import (
+	"math"
+	"strconv"
+)
+
+// Compute: O(n)
+// Memory: O(n)
+
+//nolint:unused // solution LeetCode problem
 func reverse(x int) int {
 	sign := 1
 
@@ -8,7 +19,8 @@ func reverse(x int) int {
 
 	s := strconv.Itoa(x)
 	r := []rune(s)
-	for i, j := 0, len(r) - 1; i < j; {
+
+	for i, j := 0, len(r)-1; i < j; {
 		r[i], r[j] = r[j], r[i]
 		i++
 		j--
